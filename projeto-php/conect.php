4f9690f717ef3e95d883f65d0d2d5d8d15fcb180
henrique <115552341+henrixyz@users.noreply.github.com>
2024-03-22 16:27:45 -0300

    Add files via upload

--- projeto-php/conect.php
+++ projeto-php/conect.php
@@ -1,3 +1,26 @@
-<?php
+<!--  -->
 
-$conect = new PDO ("mysql:dbname=clientes; host=localhost","root","")
+<?php
+$id = $_GET['id'];//obtem sla
+
+$host = 'localhost';
+$dbname = 'aula_php';
+$user = 'root';
+$password = 'mysql2024';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password); //pega informacoes do bd
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //testa erro
+    
+    // $query = $pdo->prepare("DELETE FROM table_users WHERE id=$id");
+    // $query->execute();
+    
+    // echo "cadastro deletado ccom sucesso!";
+
+    // echo "<br><a href='readUser.php'>
+    // <button type='button'>voltar</button>
+    // </a>";
+    
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
---
+++ projeto-php/insert.php
@@ -0,0 +1,4 @@
+<?php
+$nome = $_POST['nome'];
+$email = $_POST['e-mail'];
+$nome = $_POST['nome'];
